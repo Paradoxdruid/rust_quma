36e401c6963c54c2e1cf9fa0ebdb1908ff990548
Paradoxdruid <bonham@gmail.com>
2023-05-24 15:21:11 -0600

    fix test imports

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import time
 
-from pyllelic import quma
-
 import rust_quma
+
+import python_quma
 
 start = time.perf_counter()
 rust_quma_output = rust_quma.Quma(
@@ -13,7 +13,7 @@ end = time.perf_counter()
 print(f"Rust version took {end - start:0.4f} seconds")
 
 start = time.perf_counter()
-py_quma_output = quma.Quma(
+py_quma_output = python_quma.Quma(
     ">query\nATCGTAGTCGA", ">query1\nATCGTAGTCGA\n>query2\nATCGATAGCATT"
 )
 end = time.perf_counter()
